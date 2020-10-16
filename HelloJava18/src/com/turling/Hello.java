481fe562aceb87c7ca614a64eb0f07111ed055ac
aa <aa@qq.com>
2020-10-16 16:03:19 +0800

    update....

--- HelloJava18/src/com/turling/Hello.java
+++ HelloJava18/src/com/turling/Hello.java
@@ -3,5 +3,6 @@ package com.turling;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello tiantian");
     }
 }
