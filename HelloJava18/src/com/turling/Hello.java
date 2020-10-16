3ad31add326af0a7bdf8c935cfc09e199a73cfab
aa <aa@qq.com>
2020-10-16 17:05:38 +0800

    hellojava18 project initaaa

--- HelloJava18/src/com/turling/Hello.java
+++ HelloJava18/src/com/turling/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello tiantian");
+        System.out.println("我是dev中写的一句代码");
     }
 }
